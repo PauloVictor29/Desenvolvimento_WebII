c684956abd588ef57141aa4eec6c8dd4d24f126b
Paulo Victor <pv36058@gmail.com>
2023-01-22 13:03:21 -0300

    inicial commit

---
+++ 18_Typepf.js
@@ -0,0 +1,13 @@
+function testeVar(){
+    var a = 1;
+    var b = 2;
+    var soma = a + b;
+    var nome;
+    if(nome== undefined){
+        console.log("Teste 1 -  O nome não foi inicializado");
+    }
+    if (typeof(nome) == "undefined"){
+        console.log("Teste 2 - O nome não foi inicializado");
+    }
+    console.log("O tipo do nome é %s e o tipo da soma é")
+}
